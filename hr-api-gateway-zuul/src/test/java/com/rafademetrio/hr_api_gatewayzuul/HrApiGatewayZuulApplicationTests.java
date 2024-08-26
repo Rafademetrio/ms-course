a88761885bacbce2371f53728ae9d3e30d0b754e
Rafael Lucena <rafademetrio@gmail.com>
2024-08-26 11:03:59 -0300

    Zuul api gateway, implementation and configuration

---
+++ hr-api-gateway-zuul/src/test/java/com/rafademetrio/hr_api_gatewayzuul/HrApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafademetrio.hr_api_gatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
