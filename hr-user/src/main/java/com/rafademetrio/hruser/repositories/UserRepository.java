c548cb68cdcabbbd560a15aa1e8333e7126c1f1a
Rafael Lucena <rafademetrio@gmail.com>
2024-08-28 06:32:57 -0300

    Zuul config for hr-user, findById and findByEmail on hr-user implemented

---
+++ hr-user/src/main/java/com/rafademetrio/hruser/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.rafademetrio.hruser.repositories;
+
+import com.rafademetrio.hruser.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
+
+}
